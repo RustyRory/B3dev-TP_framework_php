81712f594395aed86a027fc50687ab433d80df54
rustyrory <marada49@live.fr>
2026-04-23 10:30:58 +0200

    feat(mcp): fixes #75 - config routes et controller film api

--- cinemap-app/routes/api.php
+++ cinemap-app/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\FilmApiController;
 use Illuminate\Support\Facades\Route;
 
 // routes/api.php
+Route::get('/films', [FilmApiController::class, 'index']);
 Route::post('/auth/login', [ApiAuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/films/{film}/locations', [FilmApiController::class, 'locations']);
